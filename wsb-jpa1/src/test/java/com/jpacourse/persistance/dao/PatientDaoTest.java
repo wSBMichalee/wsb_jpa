6c9168656cde3bfea9b31fa4b931421e0e754b3f
MichałC <micukr23@gmail.com>
2025-06-11 11:26:58 +0200

    added test to findbyID method integration test

--- wsb-jpa1/src/test/java/com/jpacourse/persistance/dao/PatientDaoTest.java
+++ wsb-jpa1/src/test/java/com/jpacourse/persistance/dao/PatientDaoTest.java
@@ -1,12 +1,15 @@
 package com.jpacourse.persistance.dao;
 
+import com.jpacourse.dto.PatientTO;
 import com.jpacourse.persistance.entity.PatientEntity;
 import com.jpacourse.persistance.entity.VisitEntity;
+import com.jpacourse.service.PatientService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,5 +47,23 @@ class PatientDaoTest {
         List<VisitEntity> visits = patientDao.findVisitsByPatientId(1L);
         assertNotNull(visits);
         assertTrue(visits.size() > 0);
+    }
+
+    @Autowired
+    private PatientService patientService;
+    @Transactional
+    @Test
+    public void whenFindById_existingId_thenReturnPatientTO() {
+        Long existingId = 1L;
+
+        PatientTO patient = patientService.findById(existingId);
+
+        assertNotNull(patient, "Pacjent nie powinien być null");
+        assertEquals("Jan", patient.getFirstName());
+        assertEquals("Kowalski", patient.getLastName());
+        assertEquals("P001", patient.getPatientNumber());
+        assertEquals("+48 123 456 789", patient.getTelephoneNumber());
+        assertEquals(LocalDate.of(1980, 1, 1), patient.getDateOfBirth());
+        assertEquals(145, patient.getHeight());
     }
 }
